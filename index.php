f373f6ebf7dca4db0a65ed1b9af0d3a176d3b449
Hazim Anas <hazimanas93@gmail.com>
2015-09-20 13:04:36 +0800

    Created the file structure. Added the skeleton HTML in the index.php and home.php page.A few lines of HTML for the link to the different modes in the home.php page.

---
+++ index.php
@@ -0,0 +1,6 @@
+		<?php include('partials/header.php'); ?>
+		<h1>Welcome to the Decision Maker Web Application</h1>
+
+		<a href="home.php">Get Started!</a>
+
+		<?php include('partials/footer.php'); ?>
---
+++ partials/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+
+	<body>
